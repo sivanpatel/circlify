d383275012562b94694623d4fc75558f69e24a0b
Sivan Patel <sivan.patel@googlemail.com>
2015-11-07 16:37:52 +0000

    Text filling in circles on click - to be altered still

--- src/canvasLoader.js
+++ src/canvasLoader.js
@@ -3,9 +3,15 @@ $(function() {
   var canvas=document.getElementById("circleCanvas"),
       context = canvas.getContext('2d');
   var placedCircles = [];
-  var textValue = null;
+  var rest = null;
+  var textValue = "";
+  var letter = null;
+  var counter = 0;
   window.addEventListener('resize', resizeCanvas, false);
   window.addEventListener('click', drawCircles, true);
+  window.addEventListener('click', firstLetter, true);
+  window.addEventListener('click', restOfString, true);
+  window.addEventListener('click', textInCircle, true);
   function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -20,7 +26,7 @@ $(function() {
     context.beginPath()
     context.arc(posx, posy, 30, 0 , 2*Math.PI);
     context.stroke();
-    placedCircles.push({x: posx,y: posy});
+    placedCircles.push({x: posx,y: posy, letter: ""});
   }
 
   function getMousePosition(event) {
@@ -34,16 +40,39 @@ $(function() {
     placedCircles.forEach(function(coords) {
       posx = coords.x
       posy = coords.y
+      letter = coords.letter
       context.beginPath()
       context.arc(posx, posy, 30, 0, 2*Math.PI);
       context.stroke();
+      context.font = "16px Arial";
+      context.fillText(letter, posx, posy);
     })
   }
-
 
   $('input[type=textarea]').bind('input propertychange', function() {
     textValue = this.value;
     console.log(textValue);
   });
-});
 
+  function firstLetter() {
+    letter = textValue.charAt(0);
+    console.log(letter);
+  };
+
+  function restOfString() {
+    rest = textValue.slice(1)
+    $('input[type=textarea]').val(rest)
+    textValue=rest
+    console.log(rest)
+  }
+
+  function textInCircle() {
+    placedCircles[counter].letter = letter
+    posx = placedCircles[counter].x
+    posy = placedCircles[counter].y
+    context.font = "16px Arial";
+    context.fillText(letter, posx, posy)
+      console.log(counter);
+    counter = counter + 1
+  }
+});
