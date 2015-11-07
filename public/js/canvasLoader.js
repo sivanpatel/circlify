6c2f7c01b67aaa2ce3d30f0cdae562447a9ad493
Sivan Patel <sivan.patel@googlemail.com>
2015-11-07 22:23:45 +0000

    Some more refactoring, extracted circle and letterFill functions

--- public/js/canvasLoader.js
+++ public/js/canvasLoader.js
@@ -1,66 +1,68 @@
 $(function() {
 
   var canvas=document.getElementById("circleCanvas"),
-      context = canvas.getContext('2d');
+      context = canvas.getContext("2d");
   var placedCircles = [];
   var rest = null;
   var textValue = "";
   var letter = null;
   var counter = 0;
 
-  window.addEventListener('resize', resizeCanvas, false);
-  canvas.addEventListener('click', drawCircles, true);
+  window.addEventListener("resize", resizeCanvas, false);
+  canvas.addEventListener("click", drawCircles, true);
 
   function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight - 100;
     reDrawCircles();
-  }
+  };
+
   resizeCanvas();
   
+  function circle() {
+    context.beginPath();
+    context.arc(posx, posy, 30, 0 , 2*Math.PI, false);
+    context.fillStyle = "#A44A3F";
+    context.fill();
+    context.stroke();
+  };
+
+  function letterFill() {
+    context.font = "18px Helvetica";
+    context.fillStyle = "black";
+    context.textAlign = "center";
+    context.fillText(letter, posx, posy);
+  };
+
   function drawCircles(e){
     firstLetter();
     restOfString();
-    var pos = getMousePosition(event)
-    posx = pos.x
-    posy = pos.y - 100
-    context.beginPath()
-    context.arc(posx, posy, 30, 0 , 2*Math.PI, false);
-    context.fillStyle = '#A44A3F';
-    context.fill();
-    context.stroke();
-    context.font = "18px Arial";
-    context.fillStyle = "black";
-    context.textAlign = 'center';
-    context.fillText(letter, posx, posy)
+    var pos = getMousePosition(event);
+    posx = pos.x;
+    posy = pos.y - 100;
     placedCircles.push({x: posx,y: posy, letter: letter});
-  }
+    circle();
+    letterFill();
+  };
 
   function getMousePosition(event) {
     return {
       x: event.clientX,
       y: event.clientY 
     };
-  }
+  };
 
   function reDrawCircles() {
     placedCircles.forEach(function(coords) {
-      posx = coords.x
-      posy = coords.y
-      letter = coords.letter
-      context.beginPath()
-      context.arc(posx, posy, 30, 0, 2*Math.PI);
-      context.fillStyle = '#A44A3F';
-      context.fill();
-      context.stroke();
-      context.font = "18px Arial";
-      context.fillStyle = "black";
-      context.textAlign = 'center';
-      context.fillText(letter, posx, posy);
+      posx = coords.x;
+      posy = coords.y;
+      letter = coords.letter;
+      circle();
+      letterFill();
     })
-  }
+  };
 
-  $('input[type=textarea]').bind('input propertychange', function() {
+  $("input[type=textarea]").bind("input propertychange", function() {
     textValue = this.value;
   });
 
@@ -69,9 +71,9 @@ $(function() {
   };
 
   function restOfString() {
-    rest = textValue.slice(1)
-    $('input[type=textarea]').val(rest)
-    textValue=rest
-  }
+    rest = textValue.slice(1);
+    $("input[type=textarea]").val(rest);
+    textValue=rest;
+  };
 
 });
